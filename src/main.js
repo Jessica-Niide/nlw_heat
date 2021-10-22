03019506eaaa9214417227e07d59804d0aa359eb
Jessica Niide <jessica.niide@gmail.com>
2021-10-22 19:43:00 -0300

    aula 4: index.html atualizado, main.js criada

---
+++ src/main.js
@@ -0,0 +1,30 @@
+const userSocialMedias = {
+  github: 'Jessica-Niide',
+  youtube: '',
+  facebook: 'jessica.niide',
+  instagram: 'jessicaniide',
+  twitter: ''
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const socialMedia = li.getAttribute('class')
+    li.children[0].href = `https://${socialMedia}.com/${userSocialMedias[socialMedia]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${userSocialMedias.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userPhoto.src = data.avatar_url
+      userName.textContent = data.name
+      userGithub.href = data.url
+      userLogin.textContent = data.login
+      userBio.textContent = data.bio
+    })
+}
+getGithubProfileInfos()
